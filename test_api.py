e7e4eb5d73ba496a76a67bb23c0e57b063659748
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-02-27 14:08:27 +0700

    Add host for connect to android

--- test_api.py
+++ test_api.py
@@ -8,7 +8,7 @@ url ="http://127.0.0.1:8000/KopiSelection/"
 #img = cv2.imread('/Users/iampamungkas/Downloads/cherries.jpg',0)
 
 #image = cv2.imread("/Users/iampamungkas/Downloads/cherries.jpg")
-payload = {"image": open("/Users/iampamungkas/Downloads/coffee_fruit.jpg", "rb")}
+payload = {"image": open("/Users/iampamungkas/Downloads/dummy.jpg", "rb")}
 
 #payload = {"path": "/Users/iampamungkas/Downloads/cherries.jpg"}
 
